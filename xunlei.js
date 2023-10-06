1b7181e63f4bba258b978826075b003d82a02080
jianfree <wujianweiwu@gmail.com>
2023-10-06 23:46:17 +0800

    Update xunlei.js

--- xunlei.js
+++ xunlei.js
@@ -1,24 +1,26 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.vip_info = [{
-    "expire_date": "20290609",
+obj.vip_info = [{   
+    "register": "19700101",
     "autodeduct": "0",
+    "daily": "100",
+    "expire": "20240101",
+    "grow": "1",
     "is_vip": "1",
-    "is_year": "1",
+    "last_pay": "20230101",
+    "level": "7",
     "pay_id": "0",
-    "pay_name": "---",
-    "register": "0",
-    "vas_id": "2",
-    "vas_type": "5",
-    "vip_day_grow": "20",
-    "vip_grow": "840",
-    "vip_level": "7",
+    "remind": "0",
+    "is_year": "1",
+    "user_vas": "2",
+    "vas_type": "0",
     "vip_icon": {
-        "general": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_a/vip_level1_deactivate.png",
-        "small": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_b/vip_lever1_deactivate.png"
-      }
-  }]
+      "general": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_a/vip_level7_deactivate.png",
+      "small": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_b/vip_lever7_deactivate.png"
+    }
+  }
+]
 console.log("xunlei response", JSON.stringify(obj))
 body = JSON.stringify(obj);
 $done({body});

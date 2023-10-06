c18151c378616d9cbce884dcb1ceb79dfccab304
jianfree <wujianweiwu@gmail.com>
2023-10-07 00:00:35 +0800

    Update xunlei.js

--- xunlei.js
+++ xunlei.js
@@ -1,8 +1,5 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.vip_info = [{   
-    "register": "19700101",
+{   
+    "register": "20210101",
     "autodeduct": "0",
     "daily": "100",
     "expire": "20240101",
@@ -10,18 +7,14 @@ obj.vip_info = [{
     "is_vip": "1",
     "last_pay": "20230101",
     "level": "7",
+    "vip_level": "7",
     "pay_id": "0",
     "remind": "0",
     "is_year": "1",
     "user_vas": "2",
-    "vas_type": "0",
+    "vas_type": "5",
     "vip_icon": {
-      "general": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_a/vip_level7_deactivate.png",
-      "small": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_b/vip_lever7_deactivate.png"
+      "general": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_a/vip_level1_deactivate.png",
+      "small": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_b/vip_lever1_deactivate.png"
     }
   }
-]
-console.log("xunlei response", JSON.stringify(obj))
-body = JSON.stringify(obj);
-$done({body});
-
